9e827ccd4e7714b2e74852f9219ac0d1859480fb
Parth_Patel <153010472+Parth2861866@users.noreply.github.com>
2025-06-01 14:34:28 -0400

    validation results and password
    creating validation results for user details and initiating conversion of password from plain text to hash text in order to store them on database. because we cannot store password as a plain text form into database

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,14 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
 
 
 module.exports.registerUser = async(req, res, next) => {
-    
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+    const { firstname, lastname, email, password } = req.body;
+    //we cannot save password as a plain text in database directly so we would have to convert it into hash text
 }
